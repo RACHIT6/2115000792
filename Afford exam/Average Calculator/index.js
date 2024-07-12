c6901f60bea1976213f708e2ef322d272866caca
RACHIT6 <89304686+RACHIT6@users.noreply.github.com>
2024-07-12 15:19:27 +0530

    Update index.js

--- Afford exam/Average Calculator/index.js
+++ Afford exam/Average Calculator/index.js
@@ -6,10 +6,28 @@ const express = require("express");
 const app = express();
 
 
-app.get ('/numbers/:numberid', (req, res)=> {
+app.get ('/numbers/:numberid', async(req, res)=> {
     const numberid = req.params.numberid;
-
-    let response = fetch ('');
+    if (numberid === 'p') {
+        let resp = await fetch('http://20.244.56.144/test/primes');
+        let data = resp.json();
+        res.send(data);
+    }
+    if (numberid === 'f') {
+        let resp = await fetch('http://20.244.56.144/test/fibo');
+        let data = resp.json();
+        res.send(data);
+    }
+    if (numberid === 'e') {
+        let resp = await fetch('http://20.244.56.144/test/even');
+        let data = resp.json();
+        res.send(data);
+    }
+    if (numberid === 'r') {
+        let resp = await fetch('http://20.244.56.144/test/rand');
+        let data = resp.json();
+        res.send(data);
+    }
 })
 
 
